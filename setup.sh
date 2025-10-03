1d7705677364aa7b84b4b5184ea7c03f586d369c
rahulak-cs-iitm <cs25d007@smail.iitm.ac.in>
2025-10-04 01:45:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 pip3 install torch torchvision torchaudio
 pip install matplotlib
 pip install wandb
+pip install pandas
 mkdir -p logs/wandb_stats
 mkdir -p logs/configurable_compression
 mkdir -p checkpoints/configurable_compression
