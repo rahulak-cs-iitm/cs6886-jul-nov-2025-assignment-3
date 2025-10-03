0fb468895f5aeb9e4a830972ae3ded03efb5dabc
rahulak-cs-iitm <cs25d007@smail.iitm.ac.in>
2025-10-04 01:35:54 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,6 @@
+pip3 install torch torchvision torchaudio
+pip install matplotlib
+pip install wandb
+mkdir -p logs/wandb_stats
+mkdir -p logs/configurable_compression
+mkdir -p checkpoints/configurable_compression
